bcbe212ace2ad0aae715a7b2cdcf2a08f0a6e78b
David Smith <dsm@dips.no>
2023-12-21 22:51:00 +0100

    Add comment; Small rewrite of doStep

--- dag8/Program.fs
+++ dag8/Program.fs
@@ -6,15 +6,21 @@ let input = File.ReadAllLines("input")
 type LR = { L: string; R: string }
 
 let commands = input[0].ToCharArray() |> List.ofArray
+
+// Parse input to build a map Location -> L/R pair with next location
+// Input:
+//  LRRLRLRLRLRRRRLRLR....
+//  
+//  GLR = (SPQ, LKJ)    <-- skip to here then build map
+//  LDM = (LXK, RMT)
+//  ...
 let lrMap =
     input
     |> Seq.skip 2
-    |> Seq.map (fun s -> s[0..2], { L=s[7..9]; R=s[12..14] })
+    |> Seq.map (fun s -> s[0..2], { L=s[7..9]; R=s[12..14] } : LR)
     |> Map.ofSeq
 
-let doStep cmd loc =
-    lrMap[loc]
-    |> (fun lr -> if cmd = 'L' then lr.L else lr.R)
+let doStep cmd loc = if cmd = 'L' then lrMap[loc].L else lrMap[loc].R
 
 let rec travel loc cmds stepsAcc =
     if loc = "ZZZ" then (loc,'.')::stepsAcc
